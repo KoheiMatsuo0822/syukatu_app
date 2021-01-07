71f57eaa37554bb1c842886794837a94920a96e8
Ubuntu <ubuntu@ip-172-31-62-232.ec2.internal>
2021-01-07 01:00:15 +0000

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,22 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get root" do
-    get root_url
-    assert_response :success
-  end
-  
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
-    assert_select "title", "ホーム | My就活"
+    assert_select "title", "My就活"
   end
   
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
-    assert_select "title", "About | My就活"
+    assert_select "title", "本サイトについて | My就活"
+  end
+  
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | My就活"
   end
 
 end
